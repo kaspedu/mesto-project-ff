86a7fd5ac5bd3ecd02cf0caba0ec3f6f721b1557
Roman Solovyev <kasp.edu@yandex.ru>
2025-03-18 23:42:56 +0300

    [спринт 5] ПР готова

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Проектная работа Mesto
-
+https://github.com/kaspedu/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardsContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function addCard(cardData, onRemove) {
+    const cardElement = cardTemplate.cloneNode(true);
+
+    const cardTitle = cardElement.querySelector('.card__title');
+    const cardImage = cardElement.querySelector('.card__image');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+
+    cardTitle.textContent = cardData.name;
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+
+    deleteButton.addEventListener('click', (event) => onRemove(event.target.closest('.card')));
+
+    cardsContainer.appendChild(cardElement);
+}
 
 // @todo: Функция удаления карточки
+function removeCard(cardElement) {
+    if (cardElement) {
+        cardElement.remove();
+    }
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(cardData => addCard(cardData, removeCard));
